08802b85a2cf6da09ea053eefc16dad9c5fade3d
Nirbhay Singh <nirbhay411@gmail.com>
2023-02-08 19:30:01 +0530

    Updated client.js

--- src/lib/client.js
+++ src/lib/client.js
@@ -1,6 +1,6 @@
 const {MongoClient} = require("mongodb")
 
-const uri = "mongodb+srv://nirbhay:MUCglyivUDpdT1iE@watches-cluster.4zay5.mongodb.net/?retryWrites=true&w=majority"
+const uri = ""
 const client = new MongoClient(uri);
 const db = client.db("watches_market_db");
 
